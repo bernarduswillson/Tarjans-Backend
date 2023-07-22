aae61f84344d877c7fc37b898fe2b49051b95175
bewe <16521486@mahasiswa.itb.ac.id>
2023-07-22 19:42:04 +0700

    feat: visual node

--- main.go
+++ main.go
@@ -2,137 +2,47 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
 )
 
-
-// GRAPH
 type Graph struct {
-	nodes int
-	adj      [][]int
+	Nodes []Node `json:"nodes"`
+	Edges []Edge `json:"edges"`
 }
 
-func NewGraph(nodes int) *Graph {
-	adj := make([][]int, nodes)
-	for i := range adj {
-		adj[i] = make([]int, 0)
-	}
+type Node struct {
+	ID    string `json:"id"`
+	Label string `json:"label"`
+}
+
+type Edge struct {
+	From string `json:"from"`
+	To   string `json:"to"`
+}
+
+func NewGraph() *Graph {
 	return &Graph{
-		nodes: nodes,
-		adj:      adj,
+		Nodes: make([]Node, 0),
+		Edges: make([]Edge, 0),
 	}
 }
 
-func (g *Graph) AddEdge(u, v int) {
-	g.adj[u] = append(g.adj[u], v)
+func (g *Graph) AddNode(id, label string) {
+	g.Nodes = append(g.Nodes, Node{
+		ID:    id,
+		Label: label,
+	})
 }
 
-
-// ALGORITHMS
-func (g *Graph) TarjanSCC() [][]int {
-	index := 0
-	lowLink := make([]int, g.nodes)
-	ids := make([]int, g.nodes)
-	onStack := make([]bool, g.nodes)
-	stack := make([]int, 0)
-	result := make([][]int, 0)
-
-	for i := 0; i < g.nodes; i++ {
-		lowLink[i] = -1
-		ids[i] = -1
-		onStack[i] = false
-	}
-
-	for i := 0; i < g.nodes; i++ {
-		if ids[i] == -1 {
-			g.DFSSCC(i, &index, &lowLink, &ids, &onStack, &stack, &result)
-		}
-	}
-
-	return result
+func (g *Graph) AddEdge(from, to string) {
+	g.Edges = append(g.Edges, Edge{
+		From: from,
+		To:   to,
+	})
 }
-
-func (g *Graph) DFSSCC(at int, index *int, lowLink, ids *[]int, onStack *[]bool, stack *[]int, result *[][]int) {
-	*index++
-	(*lowLink)[at] = *index
-	(*ids)[at] = *index
-	*stack = append(*stack, at)
-	(*onStack)[at] = true
-
-	for _, to := range g.adj[at] {
-		if (*ids)[to] == -1 {
-			g.DFSSCC(to, index, lowLink, ids, onStack, stack, result)
-			(*lowLink)[at] = min((*lowLink)[at], (*lowLink)[to])
-		} else if (*onStack)[to] {
-			(*lowLink)[at] = min((*lowLink)[at], (*ids)[to])
-		}
-	}
-
-	if (*lowLink)[at] == (*ids)[at] {
-		component := make([]int, 0)
-		for {
-			node := (*stack)[len(*stack)-1]
-			*stack = (*stack)[:len(*stack)-1]
-			(*onStack)[node] = false
-			component = append(component, node)
-			if node == at {
-				break
-			}
-		}
-		*result = append(*result, component)
-	}
-}
-
-
-func (g *Graph) DFSBridge(u, parent int, discovery, lowLink *[]int, bridges *[][]int) {
-	(*discovery)[u]++
-	(*lowLink)[u] = (*discovery)[u]
-
-	for _, v := range g.adj[u] {
-		if (*discovery)[v] == -1 {
-			(*discovery)[v] = (*discovery)[u] + 1
-			(*lowLink)[v] = (*discovery)[v]
-			g.DFSBridge(v, u, discovery, lowLink, bridges)
-			if (*lowLink)[v] > (*discovery)[u] {
-				(*bridges) = append((*bridges), []int{u, v})
-			}
-			(*lowLink)[u] = min((*lowLink)[u], (*lowLink)[v])
-		} else if v != parent {
-			(*lowLink)[u] = min((*lowLink)[u], (*discovery)[v])
-		}
-	}
-}
-
-func (g *Graph) FindBridges() [][]int {
-	discovery := make([]int, g.nodes)
-	lowLink := make([]int, g.nodes)
-	bridges := make([][]int, 0)
-
-	for i := 0; i < g.nodes; i++ {
-		discovery[i] = -1
-		lowLink[i] = -1
-	}
-
-	for i := 0; i < g.nodes; i++ {
-		if discovery[i] == -1 {
-			g.DFSBridge(i, -1, &discovery, &lowLink, &bridges)
-		}
-	}
-
-	return bridges
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 
 func main() {
 	// Enable CORS globally
@@ -187,45 +97,60 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 		// Read graph from input
 		lines := strings.Split(string(data), "\n")
-		nodeSet := make(map[byte]bool)
+		nodeSet := make(map[string]bool)
+		edges := make([]Edge, 0)
+
 		for _, line := range lines {
 			edge := strings.Split(line, " ")
 			if len(edge) != 2 {
 				continue
 			}
-			u := edge[0][0]
-			v := edge[1][0]
+			u := edge[0]
+			v := edge[1]
 			nodeSet[u] = true
 			nodeSet[v] = true
+			edges = append(edges, Edge{
+				From: u,
+				To:   v,
+			})
 		}
 
-		graph := NewGraph(len(nodeSet))
-		for _, line := range lines {
-			edge := strings.Split(line, " ")
-			if len(edge) != 2 {
-				continue
-			}
-			u := edge[0][0]
-			v := edge[1][0]
-			graph.AddEdge(int(u-'A'), int(v-'A'))
+		nodes := make([]Node, 0)
+		for node := range nodeSet {
+			nodes = append(nodes, Node{
+				ID:    node,
+				Label: node,
+			})
+		}
+
+		graph := Graph{
+			Nodes: nodes,
+			Edges: edges,
 		}
 
 		// Find SCC
-		scc := graph.TarjanSCC()
-		response := "Strongly Connected Components:<br>"
-		for _, component := range scc {
-			for _, node := range component {
-				response += fmt.Sprintf("%c ", node+'A')
+		scc := TarjanSCC(&graph)
+		sccResult := make([][]string, len(scc))
+		for i, component := range scc {
+			sccResult[i] = make([]string, len(component))
+			for j, node := range component {
+				sccResult[i][j] = node
 			}
-			response += "<br>"
 		}
 
 		// Find bridges
-		bridges := graph.FindBridges()
-		response += "<br>Bridges:<br>"
-		for _, bridge := range bridges {
-			u, v := bridge[0], bridge[1]
-			response += fmt.Sprintf("%c-%c<br>", u+'A', v+'A')
+		bridges := FindBridges(&graph)
+		bridgeResult := make([]Edge, len(bridges))
+		for i, bridge := range bridges {
+			bridgeResult[i] = bridge
+		}
+
+		response := map[string]interface{}{
+			"result": sccResult,
+			"graph": map[string]interface{}{
+				"nodes": nodes,
+				"edges": bridgeResult,
+			},
 		}
 
 		// Print the result to console log
@@ -233,10 +158,119 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 		// Send the response
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"result": response,
-		})
+		json.NewEncoder(w).Encode(response)
 	} else {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+
+func TarjanSCC(graph *Graph) [][]string {
+	index := 0
+	lowLink := make(map[string]int)
+	ids := make(map[string]int)
+	onStack := make(map[string]bool)
+	stack := make([]string, 0)
+	result := make([][]string, 0)
+
+	for _, node := range graph.Nodes {
+		lowLink[node.ID] = -1
+		ids[node.ID] = -1
+		onStack[node.ID] = false
+	}
+
+	for _, node := range graph.Nodes {
+		if ids[node.ID] == -1 {
+			DFSSCC(graph, node.ID, &index, &lowLink, &ids, &onStack, &stack, &result)
+		}
+	}
+
+	return result
+}
+
+func DFSSCC(graph *Graph, at string, index *int, lowLink, ids *map[string]int, onStack *map[string]bool, stack *[]string, result *[][]string) {
+	*index++
+	(*lowLink)[at] = *index
+	(*ids)[at] = *index
+	*stack = append(*stack, at)
+	(*onStack)[at] = true
+
+	for _, edge := range graph.Edges {
+		if edge.From != at {
+			continue
+		}
+		to := edge.To
+		if (*ids)[to] == -1 {
+			DFSSCC(graph, to, index, lowLink, ids, onStack, stack, result)
+			(*lowLink)[at] = min((*lowLink)[at], (*lowLink)[to])
+		} else if (*onStack)[to] {
+			(*lowLink)[at] = min((*lowLink)[at], (*ids)[to])
+		}
+	}
+
+	if (*lowLink)[at] == (*ids)[at] {
+		component := make([]string, 0)
+		for {
+			node := (*stack)[len(*stack)-1]
+			*stack = (*stack)[:len(*stack)-1]
+			(*onStack)[node] = false
+			component = append(component, node)
+			if node == at {
+				break
+			}
+		}
+		*result = append(*result, component)
+	}
+}
+
+func FindBridges(graph *Graph) []Edge {
+	discovery := make(map[string]int)
+	lowLink := make(map[string]int)
+	bridges := make([]Edge, 0)
+
+	for _, node := range graph.Nodes {
+		discovery[node.ID] = -1
+		lowLink[node.ID] = -1
+	}
+
+	for _, node := range graph.Nodes {
+		if discovery[node.ID] == -1 {
+			DFSBridge(graph, node.ID, "", &discovery, &lowLink, &bridges)
+		}
+	}
+
+	return bridges
+}
+
+func DFSBridge(graph *Graph, u, parent string, discovery, lowLink *map[string]int, bridges *[]Edge) {
+	(*discovery)[u]++
+	(*lowLink)[u] = (*discovery)[u]
+
+	for _, edge := range graph.Edges {
+		if edge.From != u {
+			continue
+		}
+		v := edge.To
+		if (*discovery)[v] == -1 {
+			(*discovery)[v] = (*discovery)[u] + 1
+			(*lowLink)[v] = (*discovery)[v]
+			DFSBridge(graph, v, u, discovery, lowLink, bridges)
+			if (*lowLink)[v] > (*discovery)[u] {
+				(*bridges) = append((*bridges), Edge{
+					From: u,
+					To:   v,
+				})
+			}
+			(*lowLink)[u] = min((*lowLink)[u], (*lowLink)[v])
+		} else if v != parent {
+			(*lowLink)[u] = min((*lowLink)[u], (*discovery)[v])
+		}
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
