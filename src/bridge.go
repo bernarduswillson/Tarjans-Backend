057f073dc66c4190536c08729017fd3ffc179dfa
bewe <16521486@mahasiswa.itb.ac.id>
2023-08-07 10:38:00 +0700

    fix: bridge

--- src/bridge.go
+++ src/bridge.go
@@ -4,7 +4,7 @@ func (g *Graph) FindBridges() [][]int {
 	discovery := make([]int, g.nodes)
 	lowLink := make([]int, g.nodes)
 	bridges := make([][]int, 0)
-	
+
 	for i := 0; i < g.nodes; i++ {
 		discovery[i] = -1
 		lowLink[i] = -1
@@ -12,7 +12,8 @@ func (g *Graph) FindBridges() [][]int {
 
 	for i := 0; i < g.nodes; i++ {
 		if discovery[i] == -1 {
-			g.DFSBridge(i, -1, &discovery, &lowLink, &bridges)
+			parent := -1
+			g.DFSBridge(i, parent, &discovery, &lowLink, &bridges)
 		}
 	}
 
@@ -28,9 +29,11 @@ func (g *Graph) DFSBridge(u, parent int, discovery, lowLink *[]int, bridges *[][
 			(*discovery)[v] = (*discovery)[u] + 1
 			(*lowLink)[v] = (*discovery)[v]
 			g.DFSBridge(v, u, discovery, lowLink, bridges)
+
 			if (*lowLink)[v] > (*discovery)[u] {
 				(*bridges) = append((*bridges), []int{u, v})
 			}
+
 			(*lowLink)[u] = min((*lowLink)[u], (*lowLink)[v])
 		} else if v != parent {
 			(*lowLink)[u] = min((*lowLink)[u], (*discovery)[v])
